acb82943d40b2f6580da3a650eb11ef54b108168
gklim516 <guankeng.lim@gmail.com>
2019-05-28 21:48:36 +0800

    Completed JavaScript Drum Kit

---
+++ Drum Kit/js/script.js
@@ -0,0 +1,18 @@
+const keys = document.querySelectorAll('.key');
+
+function playSound(e) {
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+    if (!audio) return; //stop the function from running
+    audio.currentTime = 0;
+    audio.play();
+    key.classList.add('playing');
+}
+
+function removeTransition(e) {
+    if (e.propertyName !== 'transform') return;
+    this.classList.remove('playing');
+}
+
+window.addEventListener('keydown', playSound);
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
